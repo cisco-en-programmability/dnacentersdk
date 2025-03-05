2a5ac98374560ee2a9666752c3ee4eddef1654fd
JosePabloOcampo1212 <jp.ocampo1000@gmail.com>
2025-03-05 12:07:27 -0600

    Error correction in the user_and_roles module

--- dnacentersdk/api/__init__.py
+++ dnacentersdk/api/__init__.py
@@ -1623,7 +1623,7 @@ class DNACenterAPI(object):
                 Topology_v2_3_7_9(
                     self._session, object_factory, _validator
                 )
-            self.userand_roles = \
+            self.user_and_roles = \
                 UserandRoles_v2_3_7_9(
                     self._session, object_factory, _validator
                 )
