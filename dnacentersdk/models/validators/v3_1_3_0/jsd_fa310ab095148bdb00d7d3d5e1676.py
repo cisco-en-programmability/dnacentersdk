c4e563ca6465326cd199e71ca7464a5db1dc4164
Megha Kandari <megakandari134@gmail.com>
2025-10-28 13:14:21 +0530

    fixed the validation issue for value by updating the type to array

--- dnacentersdk/models/validators/v3_1_3_0/jsd_fa310ab095148bdb00d7d3d5e1676.py
+++ dnacentersdk/models/validators/v3_1_3_0/jsd_fa310ab095148bdb00d7d3d5e1676.py
@@ -106,7 +106,7 @@ class JSONSchemaValidatorFa310Ab095148Bdb00D7D3D5E1676(object):
                 "type": "string"
                 },
                 "value": {
-                "type": "object"
+                "type": "array"
                 }
                 },
                 "type": "object"
